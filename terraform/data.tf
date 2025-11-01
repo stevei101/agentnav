2a267bc191d7add13f1ddf9c436fe4a1d1324493
Steven Irvin <stevei101@users.noreply.github.com>
2025-11-01 10:52:50 -0500

    fix(lint): remove unused Terraform declarations

--- terraform/data.tf
+++ terraform/data.tf
@@ -3,10 +3,5 @@ data "google_project" "current" {
   project_id = var.project_id
 }
 
-# Get default service account for Cloud Run
-data "google_compute_default_service_account" "default" {
-  project = var.project_id
-}
-
 # Note: data "google_project" "project" is defined in cloud_build.tf to avoid duplication
 
